cb2e66fe21a61bbe82c36c277eeeb2ba10e520c0
Cristian Crasneanu <crasneanu.cristian@gmail.com>
2023-07-10 11:52:56 +0300

    [ Initial commit ] Add project files

---
+++ Jokes/Jokes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Jokes
+//
+//  Created by Cristian Crasneanu on 10.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
